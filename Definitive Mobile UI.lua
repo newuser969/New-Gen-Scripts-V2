72ac135cd5e08e216d7f86ea78286c35c421015c
NightMare Scripts <86247482+newuser969@users.noreply.github.com>
2024-03-29 21:53:24 -0600

    Update Definitive Mobile UI.lua

--- Definitive Mobile UI.lua
+++ Definitive Mobile UI.lua
@@ -1,42 +1,10 @@
 local Kavo = {}
 
-local Module = { 
-    Elements = {}
-}
-
 local tween = game:GetService("TweenService")
 local tweeninfo = TweenInfo.new
 local input = game:GetService("UserInputService")
 local run = game:GetService("RunService")
 local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, input:GetPlatform())
-
-if IsOnMobile then
-    if not game.CoreGui:FindFirstChild("Show") then
-        local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
-        ScreenGui.Name = "Show"
-        local QuickCapture = Instance.new("TextButton", ScreenGui)
-        QuickCapture.Name = "UI"
-        QuickCapture.BackgroundColor3 = Color3.fromRGB(85, 0, 255)
-        QuickCapture.BackgroundTransparency = 0.14
-        QuickCapture.Position = UDim2.new(0.465, 0, 0, 40)
-        QuickCapture.Size = UDim2.new(0, 100, 0, 33)
-        QuickCapture.Font = Enum.Font.SourceSansBold
-        QuickCapture.Text = "O/C"
-        QuickCapture.TextColor3 = Color3.fromRGB(255, 0, 0)
-        QuickCapture.TextSize = 20.000
-        QuickCapture.Style = Enum.ButtonStyle.RobloxButtonDefault
-        QuickCapture.TextWrapped = true
-        QuickCapture.Draggable = true
-        
-        QuickCapture.TouchTap:Connect(function()
-            if MainWindow.Visible == false then
-                MainWindow.Visible = true
-            else
-                MainWindow.Visible = false
-            end
-        end)
-    end
-end
 
 local Utility = {}
 local Objects = {}
@@ -311,6 +279,35 @@ function Kavo.CreateLib(kavName, themeList)
     close.Image = "rbxassetid://3926305904"
     close.ImageRectOffset = Vector2.new(284, 4)
     close.ImageRectSize = Vector2.new(24, 24)
+
+    if IsOnMobile then
+        if not game.CoreGui:FindFirstChild("Show") then
+            local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
+            ScreenGui.Name = "Show"
+            local QuickCapture = Instance.new("TextButton", ScreenGui)
+            QuickCapture.Name = "UI"
+            QuickCapture.BackgroundColor3 = Color3.fromRGB(85, 0, 255)
+            QuickCapture.BackgroundTransparency = 0.14
+            QuickCapture.Position = UDim2.new(0.465, 0, 0, 40)
+            QuickCapture.Size = UDim2.new(0, 100, 0, 33)
+            QuickCapture.Font = Enum.Font.SourceSansBold
+            QuickCapture.Text = "O/C"
+            QuickCapture.TextColor3 = Color3.fromRGB(255, 0, 0)
+            QuickCapture.TextSize = 20.000
+            QuickCapture.Style = Enum.ButtonStyle.RobloxButtonDefault
+            QuickCapture.TextWrapped = true
+            QuickCapture.Draggable = true
+            
+            QuickCapture.TouchTap:Connect(function()
+                if ScreenGui.Visible == false then
+                    ScreenGui.Visible = true
+                else
+                    ScreenGui.Visible = false
+                end
+            end)
+        end
+    end
+
     close.MouseButton1Click:Connect(function()
         game.TweenService:Create(close, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {
             ImageTransparency = 1
@@ -805,119 +802,6 @@ function Kavo.CreateLib(kavName, themeList)
                     btnInfo.Text = newTitle
                 end
                 return ButtonFunction
-            end
-            
-            --functions stolen from orion
-
-            local function Create(Name, Properties, Children)
-                local Object = Instance.new(Name)
-                for i, v in next, Properties or {} do
-                    Object[i] = v
-                end
-                for i, v in next, Children or {} do
-                    v.Parent = Object
-                end
-                return Object
-            end
-            
-            local function CreateElement(ElementName, ElementFunction)
-                Module.Elements[ElementName] = function(...)
-                    return ElementFunction(...)
-                end
-            end            
-
-            local function MakeElement(ElementName, ...)
-                local NewElement = Module.Elements[ElementName](...)
-                return NewElement
-            end
-            
-            local function SetProps(Element, Props)
-                table.foreach(Props, function(Property, Value)
-                    Element[Property] = Value
-                end)
-                return Element
-            end
-            
-            local function SetChildren(Element, Children)
-                table.foreach(Children, function(_, Child)
-                    Child.Parent = Element
-                end)
-                return Element
-            end
-
-            local NotificationHolder = SetProps(SetChildren(MakeElement("TFrame"), {
-                SetProps(MakeElement("List"), {
-                    HorizontalAlignment = Enum.HorizontalAlignment.Center,
-                    SortOrder = Enum.SortOrder.LayoutOrder,
-                    VerticalAlignment = Enum.VerticalAlignment.Bottom,
-                    Padding = UDim.new(0, 5)
-                })
-            }), {
-                Position = UDim2.new(1, -25, 1, -25),
-                Size = UDim2.new(0, 300, 1, -25),
-                AnchorPoint = Vector2.new(1, 1),
-                Parent = Orion
-            })
-
-            function Elements:MakeNotification(NotificationConfig)
-                spawn(function()
-                    NotificationConfig.Name = NotificationConfig.Name or "Notification"
-                    NotificationConfig.Content = NotificationConfig.Content or "Test"
-                    NotificationConfig.Image = NotificationConfig.Image or "rbxassetid://4384403532"
-                    NotificationConfig.Time = NotificationConfig.Time or 15
-            
-                    local NotificationParent = SetProps(MakeElement("TFrame"), {
-                        Size = UDim2.new(1, 0, 0, 0),
-                        AutomaticSize = Enum.AutomaticSize.Y,
-                        Parent = NotificationHolder
-                    })
-            
-                    local NotificationFrame = SetChildren(SetProps(MakeElement("RoundFrame", Color3.fromRGB(25, 25, 25), 0, 10), {
-                        Parent = NotificationParent, 
-                        Size = UDim2.new(1, 0, 0, 0),
-                        Position = UDim2.new(1, -55, 0, 0),
-                        BackgroundTransparency = 0,
-                        AutomaticSize = Enum.AutomaticSize.Y
-                    }), {
-                        MakeElement("Stroke", Color3.fromRGB(93, 93, 93), 1.2),
-                        MakeElement("Padding", 12, 12, 12, 12),
-                        SetProps(MakeElement("Image", NotificationConfig.Image), {
-                            Size = UDim2.new(0, 20, 0, 20),
-                            ImageColor3 = Color3.fromRGB(240, 240, 240),
-                            Name = "Icon"
-                        }),
-                        SetProps(MakeElement("Label", NotificationConfig.Name, 15), {
-                            Size = UDim2.new(1, -30, 0, 20),
-                            Position = UDim2.new(0, 30, 0, 0),
-                            Font = Enum.Font.GothamBold,
-                            Name = "Title"
-                        }),
-                        SetProps(MakeElement("Label", NotificationConfig.Content, 14), {
-                            Size = UDim2.new(1, 0, 0, 0),
-                            Position = UDim2.new(0, 0, 0, 25),
-                            Font = Enum.Font.GothamSemibold,
-                            Name = "Content",
-                            AutomaticSize = Enum.AutomaticSize.Y,
-                            TextColor3 = Color3.fromRGB(200, 200, 200),
-                            TextWrapped = true
-                        })
-                    })
-            
-                    TweenService:Create(NotificationFrame, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {Position = UDim2.new(0, 0, 0, 0)}):Play()
-            
-                    wait(NotificationConfig.Time - 0.88)
-                    TweenService:Create(NotificationFrame.Icon, TweenInfo.new(0.4, Enum.EasingStyle.Quint), {ImageTransparency = 1}):Play()
-                    TweenService:Create(NotificationFrame, TweenInfo.new(0.8, Enum.EasingStyle.Quint), {BackgroundTransparency = 0.6}):Play()
-                    wait(0.3)
-                    TweenService:Create(NotificationFrame.UIStroke, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {Transparency = 0.9}):Play()
-                    TweenService:Create(NotificationFrame.Title, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {TextTransparency = 0.4}):Play()
-                    TweenService:Create(NotificationFrame.Content, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {TextTransparency = 0.5}):Play()
-                    wait(0.05)
-            
-                    NotificationFrame:TweenPosition(UDim2.new(1, 20, 0, 0),'In','Quint',0.8,true)
-                    wait(1.35)
-                    NotificationFrame:Destroy()
-                end)
             end
 
             function Elements:NewTextBox(tname, tTip, callback)
